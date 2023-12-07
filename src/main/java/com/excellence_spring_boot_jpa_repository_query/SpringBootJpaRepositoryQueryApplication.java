2cde3b268d45acd815466660a22730be71eefb48
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-07 10:55:21 +0530

    first commit

---
+++ src/main/java/com/excellence_spring_boot_jpa_repository_query/SpringBootJpaRepositoryQueryApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_jpa_repository_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaRepositoryQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaRepositoryQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_spring_boot_jpa_repository_query/SpringBootJpaRepositoryQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_jpa_repository_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaRepositoryQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
